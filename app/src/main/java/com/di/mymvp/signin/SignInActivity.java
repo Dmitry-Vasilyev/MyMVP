740be2fe8d9a7a942e278f6714b81a965ec37796
DimavLinux <dimav.programming@gmail.com>
2018-07-27 14:49:48 +0300

    save

--- app/src/main/java/com/di/mymvp/signin/SignInActivity.java
+++ app/src/main/java/com/di/mymvp/signin/SignInActivity.java
@@ -4,12 +4,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.di.mymvp.R;
+import com.di.mymvp.util.ActivityUtils;
 
 public class SignInActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_signin);
+
+        SignInFragment signInFragment = (SignInFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.contentFrame);
+        if(signInFragment == null) {
+            //Create fragment
+            signInFragment = SignInFragment.newInstance();
+            ActivityUtils.addFragmentToActivity(
+                    getSupportFragmentManager(), signInFragment, R.id.contentFrame);
+        }
+        
     }
 }
